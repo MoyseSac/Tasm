961c7d3732cdfc36e76c34bd839fd1bc538efa9a
MoyseSac <99836008+MoyseSac@users.noreply.github.com>
2024-08-25 19:52:38 -0600

    uprec.asm

--- rec.asm
+++ rec.asm
@@ -2,42 +2,47 @@
 .stack 100h 
 .data 
 buffer db 0
-rec db 2 dup('$')
+recNum db 5 dup('$')
+recLet db 5 dup('$')
 .code 
 
-mosm proc 
+mosmen proc 
     push ax 
     mov ah, 09 
     int 21h 
     pop ax
     ret 
-mosm endp 
+mosmen endp 
 
-puerto proc 
+puerto proc  
+push ax 
+push dx 
 mov ah, 00h 
-mov al, 11110011b 
+mov al, 11100011b 
 mov dx, 00h
 int 14h 
+pop dx 
+pop ax 
 ret
 puerto endp
 
 recibir_dato proc 
-push ax 
 mov ah,02h
 mov dx, 00h
 int 14h 
-mov rec,al
-pop ax 
 ret
 recibir_dato endp 
 
 estado_puerto proc
 push ax
 push dx  
-mov ah, 03h 
-mov dx, 00h
-int 14h 
-mov buffer, ah 
+recb_ciclo: 
+	mov ah, 03h 
+	mov dx, 00h
+	int 14h 
+	test ah, 02h
+jnz recb_ciclo
+
 pop ax 
 pop dx 
 ret
@@ -59,16 +64,56 @@ push dx
 ret 
 delay endp 
 
+
+recibir_string proc 
+	push si 
+	push cx 
+	lea si, recNum
+	mov cx,4
+	cicloCopiar: 
+	call estado_puerto
+	call recibir_dato
+	mov [si], al 
+	inc si
+	dec cx 
+	jnz cicloCopiar
+	pop si 
+	pop cx
+	ret 
+recibir_string endp 
+
+
+recibir_let proc 
+	push si 
+	push cx 
+	lea si, recLet
+	mov cx,4
+	cicloPasar: 
+	call estado_puerto
+	call recibir_dato
+	mov [si], al 
+	inc si
+	dec cx 
+	jnz cicloPasar
+	pop si 
+	pop cx
+	ret 
+recibir_let endp 
+
+
 start: 
 mov dx, @data 
 mov ds, dx 
 call puerto 
-call recibir_dato
+call delay
+call recibir_string
 call delay 
-lea dx, rec
-call mosm 
-
+call delay 
+call recibir_let
+lea dx, recLet 
+call mosmen
+lea dx, recNum
+call mosmen
 mov ax, 4c00h 
 int 21h 
 end start
-
